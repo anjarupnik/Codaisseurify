f481905db2f3eca3a541976df44b8b50dfa17ddd
anjarupnik <anjarupnik@hotmail.com>
2017-10-14 15:42:29 +0200

    create song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,29 @@
 class SongsController < ApplicationController
 
+before_action :set_artist
+
   def new
-    artist = Artist.find(params[:artist_id])
-    @song = artist.songs.build
+    @song = @artist.songs.build
+  end
+
+  def create
+    @song = @artist.songs.create(song_params)
+
+    if @song.save
+      redirect_to @artist
+    else
+      render "new"
+    end
   end
 
   private
 
     def song_params
-      params.require(:song).permit(:name, :link)
+      params.require(:song).permit(:name, :link, :artist_id)
+    end
+
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
     end
 
 end
