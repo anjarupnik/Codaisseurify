adcd4bc3b191954912cbf8faf9497c3b51a5e0b9
anjarupnik <anjarupnik@hotmail.com>
2017-10-14 14:02:49 +0200

    created artist show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,5 +3,10 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+    @songs = @artist.songs.all
+  end
   
 end
