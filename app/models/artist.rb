c33b8a9cb4ea1f0314b8a0da60f5fa63855597e0
anjarupnik <anjarupnik@hotmail.com>
2017-10-14 16:37:25 +0200

    validations for artist and songs

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,8 @@ class Artist < ApplicationRecord
 
   has_many :songs, dependent: :destroy
   has_one  :photo, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,9 @@
 class Song < ApplicationRecord
 
   belongs_to :artist
-  
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { maximum: 20 }, allow_blank: true
+
 end
